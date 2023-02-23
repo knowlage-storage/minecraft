fb402f53dd5a9d635dd74003c8391143ff1420b8
Ilyuschenko Pavel <pavel.ks1996@gmail.com>
2023-02-23 23:08:15 +0300

    Added tunnel length

--- tunnel.lua
+++ tunnel.lua
@@ -1,12 +1,24 @@
+local Args = { ... }
+
+if #Args = 0
+then
+    print("Need set tunnel length")
+    return
+end
+
+local length = Args[1]
+
 turtle.refuel()
 
-while true do
+while length > 0 do
 
     while turtle.detect() do
         turtle.dig()
     end
 
     turtle.forward()
+
+    length = length - 1
 
     turtle.digDown()
 
